154a958212765cfc63555d4fb07c8bd9463a4ffb
Artur <tondamus@gmail.com>
2019-05-25 16:09:56 +0200

    Created enemies animations.
    Created method for start and enemies animations.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Media.Animation;
+using System.Windows.Controls.Primitives;
 
 namespace FirstProject
 {
@@ -20,9 +23,39 @@ namespace FirstProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddEnemy();
+        }
+
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, PlayArea.ActualWidth - 50, "(Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)PlayArea.ActualHeight - 50), random.Next((int)PlayArea.ActualHeight - 50), "(Canvas.Top)");
+            PlayArea.Children.Add(enemy);
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyBoard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(propertyToAnimate));
+            storyBoard.Children.Add(animation);
+            storyBoard.Begin();
         }
     }
 }
